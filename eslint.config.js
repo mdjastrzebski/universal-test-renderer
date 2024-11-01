017e2eecaa93984280332d6a19683e8b3bf44260
Maciej Jastrzebski <mdjastrzebski@gmail.com>
2024-11-01 18:16:32 +0100

    chore: setup lint

---
+++ eslint.config.js
@@ -0,0 +1,20 @@
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+
+export default tseslint.config(
+    eslint.configs.recommended,
+    ...tseslint.configs.strictTypeChecked,
+    ...tseslint.configs.stylisticTypeChecked,
+    {
+        files: ['**/*.ts', '**/*.tsx'],
+        languageOptions: {
+            parserOptions: {
+                projectService: true,
+                tsconfigRootDir: import.meta.dirname,
+            },
+        },
+    },
+    {
+        ignores: ['node_modules/**', 'dist/**', "*.config.{js,ts}"],
+    }
+);
