b3e3e058cdfaba2edf512b0bf220dea204f9cd90
Maciej Jastrzebski <mdjastrzebski@gmail.com>
2024-11-08 10:28:32 +0100

    refactor: rename to match React-DOM

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
-export { createRenderer } from "./renderer";
+export { createRoot } from "./renderer";
 export { CONTAINER_TYPE } from "./constants";
 
-export type { Renderer, RendererOptions } from "./renderer";
+export type { Root, RootOptions } from "./renderer";
 export type { HostElement, HostElementProps, HostNode } from "./host-element";
 export type { JsonElement, JsonNode } from "./render-to-json";
