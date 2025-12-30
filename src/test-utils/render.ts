519d88011205b241836a1f50f33068ef98778447
Maciej Jastrzębski <mdjastrzebski@gmail.com>
2025-12-30 22:17:11 +0100

    .

--- src/test-utils/render.ts
+++ src/test-utils/render.ts
@@ -4,8 +4,8 @@ import { act } from "react";
 import type { Root } from "../renderer";
 
 export async function renderWithAct(root: Root, element: ReactElement) {
-    // eslint-disable-next-line @typescript-eslint/require-await -- intentionally triggering async act variant
-    await act(async () => {
-      root.render(element);
-    });
-  }
+  // eslint-disable-next-line @typescript-eslint/require-await -- intentionally triggering async act variant
+  await act(async () => {
+    root.render(element);
+  });
+}
