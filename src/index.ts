b9cead56e9ac027a98927696373cd6b86476d14e
Maciej Jastrzębski <mdjastrzebski@gmail.com>
2025-12-31 18:07:05 +0100

    fix: add missing exports

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
 export { createRoot } from "./renderer";
+export { findAll } from "./find-all";
 
 export type { Root, RootOptions } from "./renderer";
 export type { HostElement, HostElementProps, HostNode } from "./host-element";
+export type { ContainerElement } from "./container-element";
 export type { JsonElement, JsonNode } from "./render-to-json";
+export type { FindAllOptions } from "./find-all";
