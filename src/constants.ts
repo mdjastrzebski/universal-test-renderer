813539b036461c78dc570b59310de0ffa1192c22
Maciej Jastrzebski <mdjastrzebski@gmail.com>
2026-01-01 10:57:59 +0100

    fix: expose `Fiber` type from react-reconciler (#22)
    * fix: expose Fiber type
    
    * tweaks

--- src/constants.ts
+++ src/constants.ts
@@ -3,10 +3,3 @@ export const Tag = {
   Instance: "INSTANCE",
   Text: "TEXT",
 } as const;
-
-// Source: https://github.com/facebook/react/blob/main/packages/react-reconciler/src/ReactWorkTags.js#L46
-export const FiberTag = {
-  FunctionComponent: 0,
-  Root: 3,
-  HostComponent: 5,
-} as const;
