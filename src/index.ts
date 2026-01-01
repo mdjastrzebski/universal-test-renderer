a3695c12ce7eeefb8cebb654f99be76642d4de78
Maciej Jastrzębski <mdjastrzebski@gmail.com>
2026-01-01 10:24:37 +0100

    fix: expose Fiber type

--- src/index.ts
+++ src/index.ts
@@ -5,3 +5,5 @@ export type { Root, RootOptions } from "./renderer";
 export type { HostElement, HostElementProps, HostNode } from "./host-element";
 export type { JsonElement, JsonNode } from "./render-to-json";
 export type { FindAllOptions } from "./find-all";
+
+export type { Fiber } from "react-reconciler";
