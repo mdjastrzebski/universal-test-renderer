99923633d45fd6135aa74dfc05eb6c1b836d14ee
Maciej Jastrzębski <mdjastrzebski@gmail.com>
2026-01-07 09:49:59 +0100

    .

--- jest.config.js
+++ jest.config.js
@@ -3,4 +3,5 @@ export default {
   testEnvironment: "node",
   testMatch: ["**/__tests__/**/*.ts?(x)", "**/?(*.)+(spec|test).ts?(x)"],
   moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+  coveragePathIgnorePatterns: ["/node_modules/", "/test-utils/"],
 };
